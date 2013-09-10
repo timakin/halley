54dfffad408b20993f082ffc264507960224a054
timakin <timaki.st@gmail.com>
2013-09-11 00:35:59 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Halley::Application.config.secret_key_base = '5a8d40361c59ebfd534891914d1684eb73aa5c6f5dbe8c726c87ebcc98c5e0c2e67d396cf7b8167b2076e4fce3d12ff7424b068e5e256f81dd18f1491744b18b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Halley::Application.config.session_store :cookie_store, key: '_halley_session'
