a6acc7e2af2c4ebd45c4253cd625d58660a04968
timakin <timaki.st@gmail.com>
2013-11-19 13:24:16 +0900

    デザイン以外実装完了

--- app/assets/javascripts/databoxes.js
+++ app/assets/javascripts/databoxes.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+//
+//
+
+$(function(){
+	$("#databox_data").dropzone({
+		addRemoveLinks: true,
+		clickable: true,
+		createImageThumbnails: true,
+		previewContainer: ".dropzone-custom",
+		proccessing: true
+		
+	});
+});
+
+
+
